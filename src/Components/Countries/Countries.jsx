92e907072804b612672c0515c57ff65434556ecb
zisanahmed97 <zisanahmed97@gmail.com>
2026-08-06 00:13:18 +0600

    added css

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import Country from './Country';
+import  "../Countries/Countries.css"
 
 const Countries = ({countriesPromise}) => {
 
@@ -8,13 +9,15 @@ const Countries = ({countriesPromise}) => {
 
     console.log(countries);
     return (
-        <div>
+        <div >
             <h2>In your dream country</h2>
              <h2>Countries:{countries.length}</h2>
-
+<div className="countries">
+    
              {
                 countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
              }
+</div>
         </div>
     );
 };
--- src/Components/Countries/Country.jsx
+++ src/Components/Countries/Country.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import  "../Countries/Country.css"
 
 const Country = ({country}) => {
-    
+   const handleClick = () =>{
+    console.log("clicked")
+   }
     return (
-        <div>
+        <div className="country">
              <img src={country.flags.flags.png} alt="" />
             <h2>name:{country.name.common}</h2>
             <h4>Capital:{country.capital.capital}</h4>
+            <p>Area:{country.area.area} <br />
+                {
+                    country.area.area > 3000 ? "big country" : "small country"
+                }
+            </p>
+             <button onClick={handleClick}>not visited</button>
         </div>
     );
 };
